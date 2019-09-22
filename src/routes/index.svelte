ef2a6dc66c92965457760d4622d13e5f0818bd65
Geoffrey Hug <hug.geoffrey@gmail.com>
2019-09-22 20:35:40 +0200

    Sapper with hot reloading, postcss and scss

---
+++ src/routes/index.svelte
@@ -0,0 +1,62 @@
+<style type="text/scss">
+  h1,
+  figure,
+  p {
+    text-align: center;
+    margin: 0 auto;
+  }
+
+  h1 {
+    font-size: 2.8em;
+    text-transform: uppercase;
+    font-weight: 700;
+    margin: 0 0 0.5em 0;
+  }
+
+  figure {
+    margin: 0 0 1em 0;
+  }
+
+  img {
+    width: 100%;
+    max-width: 400px;
+    margin: 0 0 1em 0;
+  }
+
+  .sass {
+    h1 {
+      color: red;
+    }
+  }
+
+  p {
+    margin: 1em auto;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 4em;
+    }
+  }
+</style>
+
+<svelte:head>
+  <title>Sapper project template</title>
+</svelte:head>
+
+<h1>Great success!</h1>
+
+<div class="sass">
+  <h1>Hello</h1>
+</div>
+
+<figure>
+  <img alt="Borat" src="great-success.png" />
+  <figcaption>HIGH FIVE!</figcaption>
+</figure>
+
+<p>
+  <strong>
+    Try edit this file (src/routes/index.svelte) to test live reloading.
+  </strong>
+</p>
